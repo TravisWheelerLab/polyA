f1c1d374678989e0931ff7a286c71a98f474e487
George Lesica <george@lesica.com>
2020-03-13 15:29:09 -0600

    Add validation tests

--- test_inputs/RunTests.sh
+++ test_inputs/RunTests.sh
@@ -1,19 +1,67 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
+## ---------
+## UTILITIES
+## ---------
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NOCOLOR='\033[00m'
+
+BALLOTX='✘'
+CHECKMARK='✔'
+
+function assert-same() {
+  local base_file="$1"
+  local pl_file="$base_file.pl.output"
+  local py_file="$base_file.py.output"
+  local diff_file="$base_file.output.diff"
+  local output_diff
+  output_diff=$(diff "$pl_file" "$py_file")
+  if [ "$output_diff" == "" ]; then
+    echo -e "${GREEN}${CHECKMARK} $base_file ... PASSED${NOCOLOR}"
+  else
+    echo -e "${RED}${BALLOTX} $base_file ... FAILED${NOCOLOR} (see $diff_file)"
+    echo "$output_diff" > "$diff_file"
+  fi
+  rm "$pl_file"
+  rm "$py_file"
+}
+
+function run-script() {
+  local alignment="$1"
+  local interpreter="$2"
+  local extension="$3"
+  "$interpreter" "AdjudicateRegions.$extension" \
+      --lambda 0.1227 \
+      "$alignment" \
+      25p41g_edited.matrix \
+      > "$alignment.$extension.output" \
+      2>&1
+}
+
+function run-perl() {
+  run-script "$1" "perl" "pl"
+}
+
+function run-python() {
+  run-script "$1" "python" "py"
+}
+
+## -----
+## TESTS
+## -----
 
 # artificial test files most often used
-#for f in *.align ;
-#do echo $f;
-    #perl AdjudicateRegions.pl --lambda .1227 $f 25p41g_edited.matrix;
-    #python3 AdjudicateRegions.py --lambda .1227 $f 25p41g_edited.matrix;
-    #echo "-------------------------------------------------------------\n";
-#done
-
-perl AdjudicateRegions.pl --lambda .1227 seqs_fullAlu_subset2.align 25p41g_edited.matrix | tee perl-output.txt
-python3 AdjudicateRegions.py --lambda .1227 seqs_fullAlu_subset2.align 25p41g_edited.matrix | tee python-output.txt
+for alignment in *.align; do
+  run-perl "$alignment"
+  run-python "$alignment"
+  assert-same "$alignment"
+done
 
 # real test files from ISB, not as frequently used but need to use later 
-#for f in *.align.format ;
-#do echo $f;
-#perl AdjudicateRegions.pl --lambda .1227 $f 25p41g_edited.matrix;
-#echo "-------------------------------------------------------------\n";
-#done
+for alignment in *.align.format; do
+  run-perl "$alignment"
+  run-python "$alignment"
+  assert-same "$alignment"
+done
