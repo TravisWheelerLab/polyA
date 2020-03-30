f72767fa57905c37039940cfac0056ae3e05126c
George Lesica <george@lesica.com>
2020-03-30 17:19:09 -0600

    Bring options and constants into harmony

--- polyA/constants.py
+++ polyA/constants.py
@@ -1,41 +1,39 @@
-import math
-import sys
 import numpy as np
 
+DEFAULT_CHUNK_SIZE = 30
 """
 The width of the "window" used to break a sequence up into pieces.
 Measured in base pairs.
 """
-DEFAULT_CHUNK_SIZE = 30
 
-"""
-TODO: Explanation (Kaitlin)
-"""
 DEFAULT_GAP_EXT = -5
-
 """
 TODO: Explanation (Kaitlin)
 """
-DEFAULT_GAP_INIT = -25
 
+DEFAULT_GAP_INIT = -25
+"""
+TODO: Explanation (Kaitlin)
+"""
+
+DEFAULT_LAMBDA = 0.1227
+"""
+TODO: Explanation (Kaitlin)
+TODO: We don't want a default here, but then we require Easel
+"""
+
+NAN_STRING = "NaN"
 """
 The string used to represent "not a number" for serialization and
 deserialization.
 """
-NAN_STRING = "NaN"
 
+DEFAULT_CHANGE_PROB = float(np.nextafter(0, 1))
 """
-The base probability that we do not change our estimate from one
+The base probability that we change our estimate from one
 base pair to the next. This will generally be a very small value.
 """
-DEFAULT_SAME_PROB = float(np.nextafter(1, 0))
 
 # +-------------------------------------------------------------+
 # | Computed constants, do not manually modify below this line. |
 # +-------------------------------------------------------------+
-
-"""
-The base probability of changing our estimate from one family of
-sequences to another, part way through adjudicating a sequence.
-"""
-DEFAULT_CHANGE_PROB = 1.0 - DEFAULT_SAME_PROB
