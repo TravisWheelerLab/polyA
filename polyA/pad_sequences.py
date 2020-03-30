571bd3efa7369b473d491efa0e81655a0ff95d4a
George Lesica <george@lesica.com>
2020-03-30 17:15:54 -0600

    Touch up pad_sequences

--- polyA/pad_sequences.py
+++ polyA/pad_sequences.py
@@ -3,7 +3,6 @@ from typing import Iterable
 from .alignment import Alignment
 from .edges import edges
 
-
 _logger = logging.getLogger(__name__)
 
 
@@ -12,8 +11,12 @@ def pad_sequences(alignments: Iterable[Alignment]):
     Pad out sequences with "." to allow regions where sequences do not all
     have the same start and stop positions.
 
-    >>> a0 = Alignment(sequences=["a", "b"], start=1, stop=1, subfamily='', score=0, consensus_start=0, consensus_stop=0, strand='')
-    >>> a1 = Alignment(sequences=["aaa", "bbb"], start=0, stop=2, subfamily='', score=0, consensus_start=0, consensus_stop=0, strand='')
+    >>> a0 = Alignment(sequences=["a", "b"],
+    ...     start=1, stop=1, subfamily='', score=0,
+    ...     consensus_start=0, consensus_stop=0, strand='',)
+    >>> a1 = Alignment(sequences=["aaa", "bbb"],
+    ...     start=0, stop=2, subfamily='', score=0,
+    ...     consensus_start=0, consensus_stop=0, strand='',)
     >>> pad_sequences([a0, a1])
     >>> a0.sequence
     '.a.'
@@ -24,14 +27,14 @@ def pad_sequences(alignments: Iterable[Alignment]):
     >>> a1.subfamily_sequence
     'bbb'
     """
-    edgeStart, edgeStop = edges(alignments)
+    edge_start, edge_stop = edges(alignments)
 
     for alignment in alignments:
         if alignment.sequence == "":
             continue
 
-        left_pad = (alignment.start - edgeStart) * "."
-        right_pad = (edgeStop - alignment.stop) * "."
+        left_pad = (alignment.start - edge_start) * "."
+        right_pad = (edge_stop - alignment.stop) * "."
 
         alignment.sequence = left_pad + alignment.sequence + right_pad
         alignment.subfamily_sequence = (
