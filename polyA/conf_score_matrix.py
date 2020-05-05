057d267e6fd370ec2bd95520f929abecaf0030c6
George Lesica <george@lesica.com>
2020-05-05 15:11:33 -0600

    Implement fill_conf_score_matrix

--- polyA/conf_score_matrix.py
+++ polyA/conf_score_matrix.py
@@ -1,7 +1,37 @@
-from typing import Any, Callable, Dict, List, Tuple
+from typing import Dict, List, Tuple
 
+from align_score_matrix import AlignScoreMatrix
+from confidence_cm import confidence_cm
+
+ConfScoreMatrix = Dict[Tuple[int, int], float]
 """
 Typedef to represent a sparse confidence matrix implemented as a
 dictionary (for now).
 """
-ConfScoreMatrix = Dict[Tuple[int, int], float]
+
+
+def fill_conf_score_matrix(
+    align_score_matrix: AlignScoreMatrix,
+    non_empty_columns: List[int],
+    n_rows: int,
+    lambda_value: float,
+) -> ConfScoreMatrix:
+    conf_score_matrix: ConfScoreMatrix = {}
+
+    for col in non_empty_columns:
+        scores: List[float] = []
+
+        for row in range(n_rows):
+            if (row, col) in align_score_matrix:
+                scores.append(align_score_matrix[row, col])
+            else:
+                scores.append(0.0)
+
+        # TODO: Can we just return a list from confidence_cm?
+        confidence_temp = confidence_cm(lambda_value, scores).split(" ")
+
+        for row in range(n_rows):
+            if confidence_temp[row] != "0":
+                conf_score_matrix[row, col] = float(confidence_temp[row])
+
+    return conf_score_matrix
--- polyA/confidence_cm.py
+++ polyA/confidence_cm.py
@@ -11,7 +11,9 @@ def confidence_cm(lambda_value: float, region: List[float]) -> str:
     >>> confidence_cm(0.5, [1.0, 0.0, -1.0])
     '1.0 0.0 0.0'
     """
-    converted_scores = [2 ** (score / lambda_value) if score > 0 else 0.0 for score in region]
+    converted_scores = [
+        2 ** (score / lambda_value) if score > 0 else 0.0 for score in region
+    ]
     score_total = sum(converted_scores)
     confidence_values = (str(score / score_total) for score in converted_scores)
     return " ".join(confidence_values)
