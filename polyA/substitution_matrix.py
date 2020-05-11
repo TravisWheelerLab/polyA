543e61e4bb4a85a631e1fe7dc940c736e8d1edac
Kaitin Carey <kaitlin1.carey@umconnect.umt.edu>
2020-05-11 09:01:38 -0600

    update docstring for substitution_matrix update definition in docstring give doctest more realistic inputs

--- polyA/substitution_matrix.py
+++ polyA/substitution_matrix.py
@@ -6,7 +6,17 @@ We're using a tuple for the keys so that we don't have to
 do anything weird to get a value out of the matrix based on
 two characters.
 
-TODO: Explain what this is...
+Score matrix used when calculating alignment scores. Holds values to 
+give when aligning 2 letters (nucleotides). 
+
+Ex: VERY basic substitution matrix will give a score of 1 for matchinn nucleotides and 
+0 for mismatching nucleotides
+	A	C	T	G
+A	1	0	0	0
+C	0	1	0	0
+T	0 	0	1	0
+G	0	0	0	1
+
 """
 
 
@@ -14,14 +24,14 @@ def load_substitution_matrix(file: TextIO,) -> SubstitutionMatrix:
     """
     Load the substitution matrix along with the map of character positions.
 
-    TODO: Explain this better, like what it's used for
-    TODO: Make the doctest use realistic input values
-
+    User will input substitution matrix that is used for scoring alignments.
+    If lambda is not given, it will be calculated based on this substitution matrix.
+    
     >>> from io import StringIO
-    >>> file = StringIO("a b\\n1.0 2.0\\n3.0 4.0")
+    >>> file = StringIO("A C\\n1 0\\n0 1\\n")
     >>> s = load_substitution_matrix(file)
     >>> s
-    {('a', 'a'): 1.0, ('a', 'b'): 2.0, ('b', 'a'): 3.0, ('b', 'b'): 4.0}
+    {('A', 'A'): 1, ('A', 'C'): 0, ('C', 'A'): 0, ('C', 'C'): 1}
     """
     first_line: str = next(file).replace(" ", "").replace("\t", "").strip()
     labels: List[str] = list(first_line)
