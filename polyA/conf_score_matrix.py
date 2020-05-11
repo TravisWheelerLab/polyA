f76b8360358428b9ca4ce7037bca2786fd054145
Kaitin Carey <kaitlin1.carey@umconnect.umt.edu>
2020-05-11 17:45:22 -0600

    doc test for fill_conf_score_matrix

--- polyA/conf_score_matrix.py
+++ polyA/conf_score_matrix.py
@@ -16,17 +16,37 @@ def fill_conf_score_matrix(
     n_rows: int,
     lambda_value: float,
 ) -> ConfScoreMatrix:
-    conf_score_matrix: ConfScoreMatrix = {}
 
     """
     fills confidence matrix from alignment matrix.
     each column in the alignment matrix is a group of competing annotations that are
     input into confidence_cm, the output confidence values are used to populate the confidence 
-    matrix
+    matrix    
     
-    TODO: ask george if need a doc test here - just calls confidence_cm and uses it to 
-    populate conf matrix    
+    >>> align_mat = {(0, 0): 0, (0, 1): 100, (0, 2): 99, (1, 0): 100, (1, 1): 100, (1, 2): 100}
+    >>> non_cols = [0, 1, 2]
+    >>> conf_mat = {}
+    >>> conf_mat = fill_conf_score_matrix(align_mat, non_cols, 2, 0.1227)
+    >>> print(f"{conf_mat[0,0]:.4f}")
+    0.0000
+    >>> print(f"{conf_mat[1,0]:.4f}")
+    1.0000
+    >>> print(f"{conf_mat[0,1]:.4f}")
+    0.5000
+    >>> print(f"{conf_mat[1,1]:.4f}")
+    0.5000
+    >>> print(f"{conf_mat[0,2]:.4f}")
+    0.0035
+    >>> print(f"{conf_mat[1,2]:.4f}")
+    0.9965
+    
     """
+    
+ #        >>> print(conf_mat)
+#     {(0, 0): 0.0, (1, 0): 1.0,(0, 1): 0.5, (1, 1): 0.5, (0, 2): 0.00351, (1, 2): 0.9965}
+
+    
+    conf_score_matrix: ConfScoreMatrix = {}
 
     for col in non_empty_columns:
         scores: List[float] = []
