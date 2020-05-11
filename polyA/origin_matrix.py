031b9d67bcd7505c354e603d1d55e04c897ad10e
Kaitin Carey <kaitlin1.carey@umconnect.umt.edu>
2020-05-11 08:00:45 -0600

    add docstring to origin_matrix

--- polyA/origin_matrix.py
+++ polyA/origin_matrix.py
@@ -1,3 +1,14 @@
 from typing import Dict, Tuple
 
-OriginMatrix = Dict[Tuple[int, int], int]
+#FIXME - GEORGE - this is a collapsed matrix so I updated type to be Dict[Tuple[str, int], int]
+#FIXME might break code?
+OriginMatrix = Dict[Tuple[str, int], int]
+
+"""
+Hash implementation of sparse 2D DP matrix. This is a collapsed matrix. Key is Tuple[str, int] 
+with the row(subfam) and column. Holds which cell in previous column the probability in 
+the DP matrix came from. Used when doing backtrace through the DP matrix. 
+
+Doesn't have any functions - Populated in FillProbMatrix()
+"""
+
