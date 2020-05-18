b568c1a0f7b46de188b89a67df131e94cab91f9d
Kaitin Carey <kaitlin1.carey@umconnect.umt.edu>
2020-05-18 15:39:05 -0600

    add first pass at doc test for align_score_matrix - add more realistic alignment

--- polyA/align_score_matrix.py
+++ polyA/align_score_matrix.py
@@ -44,8 +44,19 @@ def fill_align_score_matrix(
 	Score are weighted based on number of nucleotides that contribute to the score - so beginning
 	and trailing positions with less than chunksize nucleotides don't have lower scores
 	
-	TODO: KAITLIN - doc test
-
+	TODO: KAITLIN - more robust test
+	
+	** padding of (chunksize-1)/2 added to right pad.. this way we can go all the way to the
+	end of the sequence and calc alignscores without doing anything special 
+	
+	>>> sub_mat = {("A", "A"): 1, ("A", "T"): -1, ("T", "A"): -1, ("T", "T"): 1}
+	>>> a0 = Alignment("", 0, 0, 0, -1, -1, ["", ""], "")
+    >>> a1 = Alignment("", 0, 0, 0, 0, 0, ["TAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAT...............", "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA..............."], "+")
+	>>> (matrix, col) = fill_align_score_matrix([a1], -5, -25, 0, sub_mat)
+	>>> matrix
+	{(0, 0): 27, (0, 1): 27, (0, 2): 27, (0, 3): 27, (0, 4): 27, (0, 5): 28, (0, 6): 28, (0, 7): 28, (0, 8): 28, (0, 9): 28, (0, 10): 28, (0, 11): 28, (0, 12): 28, (0, 13): 28, (0, 14): 28, (0, 15): 29, (0, 16): 31, (0, 17): 31, (0, 18): 31, (0, 19): 31, (0, 20): 31, (0, 21): 31, (0, 22): 31, (0, 23): 31, (0, 24): 29, (0, 25): 28, (0, 26): 28, (0, 27): 28, (0, 28): 28, (0, 29): 28, (0, 30): 28, (0, 31): 28, (0, 32): 28, (0, 33): 28, (0, 34): 28, (0, 35): 27, (0, 36): 27, (0, 37): 27, (0, 38): 27, (0, 39): 27}
+	>>> col
+	40
     """
     align_score_matrix: AlignScoreMatrix = {}
 
@@ -56,14 +67,15 @@ def fill_align_score_matrix(
         sequence = alignment.sequence
 
         nuc_index = alignment.start - edge_start
-        # TODO: Force chunk_size to be even?
-        score_index = nuc_index + int(chunk_size / 2)
+
+        score_index = nuc_index + int((chunk_size - 1) / 2)
 
         align_score: float = 0.0
 
         for nuc_offset in range(15, -1, -1):
             temp_index = nuc_index
             temp_count = 0
+
             while temp_count < chunk_size - nuc_offset:
                 if sequence[temp_index] != "-":
                     temp_count += 1
@@ -150,49 +162,53 @@ def fill_align_score_matrix(
                             nuc_count -= 1
 
                         if (
-                            subfamily_sequence[nuc_index + nuc_offset - 15]
+                            subfamily_sequence[nuc_index + chunk_offset - 15]
                             == "."
-                            or sequence[nuc_index + nuc_offset - 15] == "."
+                            or sequence[nuc_index + chunk_offset - 15] == "."
                         ):
                             align_score = -inf
-                        elif subfamily_sequence[nuc_index + nuc_offset] == "-":
+                        elif (
+                            subfamily_sequence[nuc_index + chunk_offset] == "-"
+                        ):
                             nuc_count += 1
                             if (
-                                subfamily_sequence[nuc_index + nuc_offset - 1]
+                                subfamily_sequence[nuc_index + chunk_offset - 1]
                                 == "-"
                             ):
                                 align_score += gap_extend_score
                             else:
                                 align_score += gap_start_score
                         elif (
-                            subfamily_sequence[nuc_index + nuc_offset] == "."
-                            or sequence[nuc_index + nuc_offset] == "."
+                            subfamily_sequence[nuc_index + chunk_offset] == "."
+                            or sequence[nuc_index + chunk_offset] == "."
                         ):
                             align_score = align_score
                         else:
                             align_score += substitution_matrix[
-                                subfamily_sequence[nuc_index + nuc_offset],
-                                sequence[nuc_index + nuc_offset],
+                                subfamily_sequence[nuc_index + chunk_offset],
+                                sequence[nuc_index + chunk_offset],
                             ]
                             nuc_count += 1
 
                     if align_score <= 0:
-                        align_score_matrix[alignment_index, nuc_index] = 1
+                        align_score_matrix[alignment_index, score_index] = 1
                     else:
-                        align_score_matrix[alignment_index, nuc_index] = int(
+                        align_score_matrix[alignment_index, score_index] = int(
                             align_score / nuc_count * chunk_size
                         )
 
                     if align_score == -inf:
-                        del align_score_matrix[alignment_index, nuc_index]
+                        del align_score_matrix[alignment_index, score_index]
                         break
+
+                score_index += 1
 
             nuc_index += 1
             prev_chunk_offset = chunk_offset
 
-        # TODO: We just need to keep track of the max nucleotide index
-        if column_count < nuc_index:
-            column_count = nuc_index
+        #         # TODO: We just need to keep track of the max nucleotide index
+        if column_count < score_index:
+            column_count = score_index
 
     return align_score_matrix, column_count
 
