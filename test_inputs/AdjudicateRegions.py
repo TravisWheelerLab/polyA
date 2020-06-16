a332eab2edea5947ad6ca6b80ba2101b97124cbf
Kaitin Carey <kaitlin1.carey@umconnect.umt.edu>
2020-06-16 11:37:15 -0600

    fix FillAlignScoreMatrix() no longer calls CalcScore() for all chunks at start of seq. now calls it once and accumulates score

--- test_inputs/AdjudicateRegions.py
+++ test_inputs/AdjudicateRegions.py
@@ -283,14 +283,13 @@ def FillAlignMatrix(edge_start: int, chunk_size: int, gap_ext: int, gap_init: in
     {(1, 2): 31.0, (1, 3): 31.0, (1, 4): 31.0, (1, 5): 31.0, (1, 6): 31.0, (1, 7): 31.0, (1, 8): 31.0, (1, 9): 31.0, (1, 10): 31.0, (1, 11): 31.0, (1, 12): 31.0, (1, 13): 31.0, (1, 14): 31.0, (1, 15): 31.0, (1, 16): 31.0, (1, 17): 31.0, (1, 18): 31.0, (1, 19): 31.0, (1, 20): 31.0, (1, 21): 31.0, (1, 22): 31.0, (1, 23): 31.0, (1, 24): 29.0, (1, 25): 28.933333333333334, (1, 26): 28.862068965517242, (1, 27): 28.78571428571429, (1, 28): 28.703703703703702, (1, 29): 28.615384615384617, (1, 30): 28.52, (1, 31): 28.416666666666664, (1, 32): 28.304347826086953, (1, 33): 28.18181818181818, (1, 34): 28.047619047619047, (1, 35): 27.900000000000002, (1, 36): 27.736842105263158, (1, 37): 27.555555555555554, (1, 38): 27.352941176470587, (1, 39): 27.125, (2, 0): 27.125, (2, 1): 27.352941176470587, (2, 2): 27.555555555555557, (2, 3): 27.736842105263158, (2, 4): 27.9, (2, 5): 28.047619047619047, (2, 6): 28.181818181818183, (2, 7): 28.304347826086957, (2, 8): 28.416666666666668, (2, 9): 28.52, (2, 10): 28.615384615384617, (2, 11): 28.703703703703702, (2, 12): 28.785714285714285, (2, 13): 28.862068965517242, (2, 14): 28.933333333333334, (2, 15): 29.0, (2, 16): 31.0, (2, 17): 31.0, (2, 18): 31.0, (2, 19): 31.0, (2, 20): 31.0, (2, 21): 31.0, (2, 22): 5.0, (2, 23): 1.0, (2, 24): 1.0, (2, 25): 1.0, (2, 26): 1.0, (2, 27): 1.0, (2, 28): 1.0, (2, 29): 1.0, (2, 30): 1.0, (2, 31): 1.0, (2, 32): 1.0, (2, 33): 1.0, (2, 34): 1.0, (2, 35): 1.0, (2, 36): 1.0, (2, 37): 1.0, (2, 38): 1.0, (2, 39): 1.0, (0, 0): 1.0, (0, 1): 1.0, (0, 2): 1.0, (0, 3): 1.0, (0, 4): 1.0, (0, 5): 1.0, (0, 6): 1.0, (0, 7): 1.0, (0, 8): 1.0, (0, 9): 1.0, (0, 10): 1.0, (0, 11): 1.0, (0, 12): 1.0, (0, 13): 1.0, (0, 14): 1.0, (0, 15): 1.0, (0, 16): 1.0, (0, 17): 1.0, (0, 18): 1.0, (0, 19): 1.0, (0, 20): 1.0, (0, 21): 1.0, (0, 22): 1.0, (0, 23): 1.0, (0, 24): 1.0, (0, 25): 1.0, (0, 26): 1.0, (0, 27): 1.0, (0, 28): 1.0, (0, 29): 1.0, (0, 30): 1.0, (0, 31): 1.0, (0, 32): 1.0, (0, 33): 1.0, (0, 34): 1.0, (0, 35): 1.0, (0, 36): 1.0, (0, 37): 1.0, (0, 38): 1.0, (0, 39): 1.0}
     """
 
-    # time1: float = time.time()
+    time1: float = time.time()
 
     num_cols: int = 0
 
     align_matrix: Dict[Tuple[int, int], float] = {}
 
     # chunks can't start on gaps and gaps don't count when getting to the 30 bps
-
     for i in range(1, len(chroms)):
         subfam_seq: str = subfams[i]
         chrom_seq: str = chroms[i]
@@ -300,42 +299,68 @@ def FillAlignMatrix(edge_start: int, chunk_size: int, gap_ext: int, gap_init: in
         # will offset by 10
 
         # calculates score for the first (chunksize-1)/2 chunks, chunk ((chunksize-1)/2)+1 is the first one that is 31nt
-        # FIXME - this could start with smallest chunk and seq_index just add score of next nt to get next, etc
-        # FIXME - but right now it takes all the chunks separately and runs them through CalcScore()
         seq_index: int = starts[i] - edge_start
         col_index = seq_index + int((chunk_size - 1) / 2)  # col_index is the col we are in the align score matrix, $seq_index is the place in @subfam_seq and @chrom_seq
-        align_score: float = 0
-        offset: int = 0
-        prev_offset: int = 0
 
-        for k in range(int((chunk_size - 1) / 2), -1, -1):
+        k = int((chunk_size - 1) / 2)
 
-            offset: int = chunk_size - k
+        temp_index = seq_index
+        temp_count = 0
 
-            temp_index = seq_index
-            temp_count = 0
+        while temp_count < chunk_size - k:
+            if chrom_seq[temp_index] != "-":
+                temp_count += 1
+            temp_index += 1
 
-            while temp_count < chunk_size - k:
-                if chrom_seq[temp_index] != "-":
-                    temp_count += 1
-                temp_index += 1
+        offset: int = temp_index - seq_index
+        prev_offset: int = offset
 
-            offset = temp_index - seq_index
-            prev_offset = offset
+        # grabs first chunk - here seq_index = pos of first non '.' char
+        chrom_slice: str = chrom_seq[seq_index:seq_index + offset]
+        subfam_slice: str = subfam_seq[seq_index:seq_index + offset]
 
-            # grabs first chunk - here seq_index = pos of first non '.' char
-            chrom_slice: str = chrom_seq[seq_index:seq_index + offset]
-            subfam_slice: str = subfam_seq[seq_index:seq_index + offset]
+        # calculates score for first chunk and puts score in align_matrix
+        align_score: float = CalcScore(gap_ext, gap_init, subfam_slice, chrom_slice, "", "", sub_matrix)
+        align_matrix[i, col_index - k] = align_score * chunk_size / (chunk_size - k)  # already to scale so don't need to * 31 and / 31
 
-            # calculates score for first chunk and puts score in align_matrix
-            align_score = CalcScore(gap_ext, gap_init, subfam_slice, chrom_slice, "", "", sub_matrix)
-            align_matrix[i, col_index - k] = align_score * chunk_size / (chunk_size - k)  # already to scale so don't need to * 31 and / 31
+        for k in range(int((chunk_size - 1) / 2)-1, -1, -1):
+
+            if chroms[i][seq_index + offset] != '-':
+
+                if subfams[i][seq_index + offset] == '-':
+                    if subfams[i][seq_index + offset-1] == '-':
+                        align_score = align_score + gap_ext
+                    else:
+                        align_score = align_score + gap_init
+                else:
+                    align_score = align_score + sub_matrix[subfams[i][seq_index + offset] + chroms[i][seq_index + offset]]
+
+                align_matrix[i, col_index - k] = align_score * chunk_size / (chunk_size - k)
+
+                offset += 1
+            else:
+
+                temp_index = seq_index
+                temp_count = 0
+
+                while temp_count < chunk_size - k:
+                    if chrom_seq[temp_index] != "-":
+                        temp_count += 1
+                    temp_index += 1
+
+                offset = temp_index - seq_index
+                prev_offset = offset
+
+                chrom_slice: str = chrom_seq[seq_index:seq_index + offset]
+                subfam_slice: str = subfam_seq[seq_index:seq_index + offset]
+
+                align_score = CalcScore(gap_ext, gap_init, subfam_slice, chrom_slice, subfams[i][seq_index-1], chroms[i][seq_index-1], sub_matrix)
+                align_matrix[i, col_index - k] = align_score * chunk_size / (chunk_size - k)
 
         col_index += 1
 
         num_nucls: int = chunk_size  # how many nucls contributed to align score
 
-        # TODO: Make sure these bounds are right since Python indexing is different
         # move to next chunk by adding next chars score and subtracting prev chars score
         while seq_index + offset < len(chrom_seq):
             temp_index = seq_index
@@ -348,61 +373,64 @@ def FillAlignMatrix(edge_start: int, chunk_size: int, gap_ext: int, gap_init: in
 
             offset = temp_index - seq_index
 
+            if chrom_seq[seq_index + 1] == ".":
+                break
+
             if chrom_seq[seq_index + 1] != "-":
-                if chrom_seq[seq_index + 1] != "." and subfam_seq[seq_index + 1] != ".":
-                    if prev_offset != offset:  # there is a new gap, or a gap was removed from beginning
-                        chrom_slice = chrom_seq[seq_index + 1:seq_index + offset + 1]
-                        subfam_slice = subfam_seq[seq_index + 1:seq_index + offset + 1]
-                        align_score = CalcScore(gap_ext, gap_init, subfam_slice, chrom_slice,
+                # if chrom_seq[seq_index + 1] != ".":
+                if prev_offset != offset:  # there is a new gap, or a gap was removed from beginning
+                    chrom_slice = chrom_seq[seq_index + 1:seq_index + offset + 1]
+                    subfam_slice = subfam_seq[seq_index + 1:seq_index + offset + 1]
+                    align_score = CalcScore(gap_ext, gap_init, subfam_slice, chrom_slice,
                                                 subfam_seq[seq_index], chrom_seq[seq_index], sub_matrix)
 
-                        temp_count2: int = 0
-                        for nuc in chrom_slice:
-                            if nuc != '-' and nuc != '.':
-                                temp_count2 += 1
-                        num_nucls = temp_count2  # resetting num_nucls to chunk_size
+                    temp_count2: int = 0
+                    for nuc in chrom_slice:
+                        if nuc != '-' and nuc != '.':
+                            temp_count2 += 1
+                    num_nucls = temp_count2  # resetting num_nucls to chunk_size
 
-                        if num_nucls <= int((chunk_size - 1) / 2):
-                            align_score = -inf
+                    if num_nucls <= int((chunk_size - 1) / 2):
+                        align_score = -inf
 
-                    else:
-                        # align_score from previous segment - prev chars score + next chars score
-                        # subtracting prev  chars score - tests if its a gap in the subfam as well
+                else:
+                    # align_score from previous segment - prev chars score + next chars score
+                    # subtracting prev  chars score - tests if its a gap in the subfam as well
 
-                        if subfam_seq[seq_index] == "-":
-                            num_nucls -= 1
-                            if subfam_seq[seq_index - 1] == "-":
-                                align_score = align_score - gap_ext
-                            else:
-                                align_score = align_score - gap_init
+                    if subfam_seq[seq_index] == "-":
+                        num_nucls -= 1
+                        if subfam_seq[seq_index - 1] == "-":
+                            align_score = align_score - gap_ext
                         else:
-                            align_score = align_score - sub_matrix[subfam_seq[seq_index] + chrom_seq[seq_index]]
-                            num_nucls -= 1
-
-                        # adding next chars score - tests if its a gap in the subfam as well
-                        if subfam_seq[seq_index + offset - int((chunk_size - 1) / 2)] == "." or chrom_seq[
-                            seq_index + offset - int((chunk_size - 1) / 2)] == ".":
-                            align_score = -inf
-                        elif subfam_seq[seq_index + offset] == "-":
-                            num_nucls += 1
-                            if subfam_seq[seq_index + offset - 1] == "-":
-                                align_score = align_score + gap_ext
-                            else:
-                                align_score = align_score + gap_init
-                        elif subfam_seq[seq_index + offset] == "." or chrom_seq[seq_index + offset] == ".":
-                            align_score = align_score
-                        else:
-                            align_score = align_score + sub_matrix[subfam_seq[seq_index + offset] + chrom_seq[seq_index + offset]]
-                            num_nucls += 1
-
-                    if align_score <= 0:
-                        align_matrix[i, col_index] = 1.0
+                            align_score = align_score - gap_init
                     else:
-                        align_matrix[i, col_index] = align_score / num_nucls * chunk_size
+                        align_score = align_score - sub_matrix[subfam_seq[seq_index] + chrom_seq[seq_index]]
+                        num_nucls -= 1
 
-                    if align_score == -inf:
-                        del align_matrix[i, col_index]
-                        break
+                    # adding next chars score - tests if its a gap in the subfam as well
+                    if subfam_seq[seq_index + offset - int((chunk_size - 1) / 2)] == "." or chrom_seq[
+                        seq_index + offset - int((chunk_size - 1) / 2)] == ".":
+                        align_score = -inf
+                    elif subfam_seq[seq_index + offset] == "-":
+                        num_nucls += 1
+                        if subfam_seq[seq_index + offset - 1] == "-":
+                            align_score = align_score + gap_ext
+                        else:
+                            align_score = align_score + gap_init
+                    elif subfam_seq[seq_index + offset] == "." or chrom_seq[seq_index + offset] == ".":
+                        align_score = align_score
+                    else:
+                        align_score = align_score + sub_matrix[subfam_seq[seq_index + offset] + chrom_seq[seq_index + offset]]
+                        num_nucls += 1
+
+                if align_score <= 0:
+                    align_matrix[i, col_index] = 1.0
+                else:
+                    align_matrix[i, col_index] = align_score / num_nucls * chunk_size
+
+                if align_score == -inf:
+                    del align_matrix[i, col_index]
+                    break
 
                 col_index += 1
 
@@ -419,6 +447,9 @@ def FillAlignMatrix(edge_start: int, chunk_size: int, gap_ext: int, gap_init: in
 
     # print("FillAlignScoreMatrix", time.time() - time1)
     # print()
+
+    # PrintMatrixHash(num_cols, len(chroms), Subfams, align_matrix)
+    # exit()
 
     return (num_cols, align_matrix)
 
@@ -1599,7 +1630,7 @@ if __name__ == "__main__":
     #used during confidence calculations
     SubfamCounts: Dict[str, float] = {}
     PriorTotal: float = 0
-    prob_skip = 0.4 # about 60% pf genome is TE derived #FIXME - do we want to hard code this in?
+    prob_skip = 0.4 # about 60% of genome is TE derived #FIXME - do we want to hard code this in?
     if infile_prior_counts:
         for line in in_counts[1:]:
             line = re.sub(r"\n", "", line)
