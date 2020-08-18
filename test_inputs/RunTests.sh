639e9a5561497e6cf1e9e9952b8eea0d97a1bb47
George Lesica <george@lesica.com>
2020-08-18 09:30:20 -0600

    Fix script formatting

--- test_inputs/RunTests.sh
+++ test_inputs/RunTests.sh
@@ -2,8 +2,8 @@
 
 set -e
 
- for f in *.align.format ;
- 	do echo "$f";
- 		poetry run python3 AdjudicateRegions.py --seqpos --lambda .1227 $f 25p41g_edited.matrix;
- 		printf -- "-------------------------------------------------------------\n";
- 	done
+for f in *.align.format ;
+do echo "$f";
+  poetry run python3 AdjudicateRegions.py --seqpos --lambda .1227 $f 25p41g_edited.matrix;
+  printf -- "-------------------------------------------------------------\n";
+done
