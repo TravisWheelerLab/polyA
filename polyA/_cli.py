1e5cd00de7ec75d345948a45d5c2e191ad2dbc16
George Lesica <george@lesica.com>
2020-09-08 13:39:18 -0600

    Clean up imports for now

--- polyA/_cli.py
+++ polyA/_cli.py
@@ -2,8 +2,6 @@ from logging import Formatter, INFO, Logger, StreamHandler, getLogger
 from typing import List
 
 from ._options import Options
-from .fill_prob_matrix import fill_prob_matrix
-from .prob_matrix import serialize_prob_matrix
 
 
 def _configure_logging(options: Options) -> None:
@@ -27,11 +25,11 @@ def run(args: List[str]) -> None:
     # For now we just compute a probability matrix from a support
     # matrix because that's all we've ported to Python so far.
 
-    probMatrix, originMatrix = fill_prob_matrix(
-        options.support_matrix,
-        columns=options.columns,
-        benchmark=options.benchmark,
-        change_prob=options.change_prob,
-        chunk_size=options.chunk_size,
-    )
-    serialize_prob_matrix(probMatrix, originMatrix)
+    # probMatrix, originMatrix = fill_prob_matrix(
+    #     options.support_matrix,
+    #     columns=options.columns,
+    #     benchmark=options.benchmark,
+    #     change_prob=options.change_prob,
+    #     chunk_size=options.chunk_size,
+    # )
+    # serialize_prob_matrix(probMatrix, originMatrix)
