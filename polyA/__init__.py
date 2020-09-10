19ac5b4ac41d870a83494bc6e8220f5dc7e90642
George Lesica <george@lesica.com>
2020-09-09 18:13:13 -0600

    Simplify ci and export stuff

--- polyA/__init__.py
+++ polyA/__init__.py
@@ -1,31 +1,31 @@
 __version__ = "0.1.0"
 
-from alignment import Alignment
-from calculate_score import calculate_score
-from collapse_matrices import collapse_matrices
-from confidence_cm import confidence_cm
-from constants import (
+from .alignment import Alignment
+from .calculate_score import calculate_score
+from .collapse_matrices import collapse_matrices
+from .confidence_cm import confidence_cm
+from .constants import (
     DEFAULT_CHANGE_PROB,
     DEFAULT_CHUNK_SIZE,
     NAN_STRING,
 )
-from edges import edges
-from extract_nodes import extract_nodes
-from fill_align_matrix import fill_align_matrix
-from fill_confidence_matrix import fill_confidence_matrix
-from fill_consensus_position_matrix import fill_consensus_position_matrix
-from fill_node_confidence import fill_node_confidence
-from fill_path_graph import fill_path_graph
-from fill_probability_matrix import fill_probability_matrix
-from fill_support_matrix import fill_support_matrix
-from get_path import get_path
-from lambda_provider import (
+from .edges import edges
+from .extract_nodes import extract_nodes
+from .fill_align_matrix import fill_align_matrix
+from .fill_confidence_matrix import fill_confidence_matrix
+from .fill_consensus_position_matrix import fill_consensus_position_matrix
+from .fill_node_confidence import fill_node_confidence
+from .fill_path_graph import fill_path_graph
+from .fill_probability_matrix import fill_probability_matrix
+from .fill_support_matrix import fill_support_matrix
+from .get_path import get_path
+from .lambda_provider import (
     ConstantLambdaProvider,
     EaselLambdaProvider,
     LambdaProvider,
 )
-from load_alignments import load_alignments
-from matrices import (
+from .load_alignments import load_alignments
+from .matrices import (
     ConsensusMatrix,
     CollapsedMatrices,
     ConsensusMatrixContainer,
@@ -34,12 +34,12 @@ from matrices import (
     RowColumn,
     StrandMatrix,
 )
-from pad_sequences import pad_sequences
-from parameters import (
+from .pad_sequences import pad_sequences
+from .parameters import (
     Parameters,
     EaselRunner,
 )
-from printers import (
+from .printers import (
     print_matrix_hash,
     print_matrix_support,
     print_results,
