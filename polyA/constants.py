17683f0a71375ed94b5df8d546f814ef9f2f935d
Kaitin Carey <kaitlin1.carey@umconnect.umt.edu>
2020-10-13 12:54:20 -0600

    update default constants

--- polyA/constants.py
+++ polyA/constants.py
@@ -1,35 +1,23 @@
-DEFAULT_CHUNK_SIZE = 30
+DEFAULT_CHUNK_SIZE = 31
 """
 The width of the "window" used to break a sequence up into pieces.
 Measured in base pairs.
 """
 
-DEFAULT_GAP_EXT = -5
-"""
-TODO: Explanation (Kaitlin)
-"""
-
 DEFAULT_GAP_INIT = -25
 """
-TODO: Explanation (Kaitlin)
+Penalty given to start a gap in alignment
 """
 
-DEFAULT_LAMBDA = 0.1227
+DEFAULT_GAP_EXT = -5
 """
-TODO: Explanation (Kaitlin)
-TODO: We don't want a default here, but then we require Easel
+Penalty given to extend a gap in alignment
 """
 
 NAN_STRING = "NaN"
 """
 The string used to represent "not a number" for serialization and
 deserialization.
-"""
-
-DEFAULT_CHANGE_PROB = 10 ** -45
-"""
-The base probability that we change our estimate from one
-base pair to the next. This will generally be a very small value.
 """
 
 # +-------------------------------------------------------------+
