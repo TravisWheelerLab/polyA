c2cea0f4ae44ccb06137afbfcc21939946ca3909
Kaitin Carey <kaitlin1.carey@umconnect.umt.edu>
2020-10-13 07:21:56 -0600

    move all test files to fixtures

--- test_inputs/RunTests.sh
+++ test_inputs/RunTests.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-for f in *.align.format ;
+for f in ../fixtures/*.align.format ;
 do echo "$f";
   python AdjudicateRegions.py --seqpos --lambda .1227 "$f" 25p41g_edited.matrix;
   printf -- "-------------------------------------------------------------\n";
