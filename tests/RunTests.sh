6a717fd8bf07052c5e77fdd8a9d7891f30e4bf46
Kaitin Carey <kaitlin1.carey@umconnect.umt.edu>
2020-10-13 07:53:36 -0600

    move matrix and subfam prior counts files to fixtures

--- tests/RunTests.sh
+++ tests/RunTests.sh
@@ -4,6 +4,6 @@ set -e
 
 for f in ../fixtures/*.align.format ;
 do echo "$f";
-  python ../test_inputs/AdjudicateRegions.py --seqpos --lambda .1227 "$f" ../test_inputs/25p41g_edited.matrix;
+  python ../test_inputs/AdjudicateRegions.py --seqpos --lambda .1227 "$f" ../fixtures/25p41g_edited.matrix;
   printf -- "-------------------------------------------------------------\n";
 done
