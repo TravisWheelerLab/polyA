f63f302040bd134b1c2b6ec1d1a973bf46172dae
audreym98 <audreym98@icloud.com>
2020-10-15 11:21:45 -0600

    Added ultra test files, updated test script for new paths, added test to Makefile

---
+++ tests/RunUltraTests.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# test with ultra output
+for f in ../fixtures/ultra_test_files/*.fa ;
+do echo "$f";
+  ultra_output="${f}.ultra";
+  align_file="${f}.cm.align";
+  python -m polyA --seqpos --lambda .1227 --ultraOutput "$ultra_output" "$align_file" ../fixtures/25p41g_edited.matrix;
+  printf -- "-------------------------------------------------------------\n";
+done
+
+#tests soda output
+for f in ../fixtures/ultra_test_files/*.fa ;
+do echo "$f";
+  ultra_output="${f}.ultra";
+  align_file="${f}.cm.align";
+  python -m polyA --seqpos --lambda .1227 --viz outfile.viz --heatmap outfile.heatmap --ultraOutput "$ultra_output" "$align_file" ../fixtures/25p41g_edited.matrix;
+  rm outfile.viz;
+  rm outfile.viz.json;
+  rm outfile.heatmap;
+  printf -- "-------------------------------------------------------------\n";
+done
