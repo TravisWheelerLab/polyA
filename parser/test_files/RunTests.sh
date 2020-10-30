7a260feba4ca5b5719d26dc7cd8d96276a12befc
Kaitin Carey <kaitlin1.carey@umconnect.umt.edu>
2020-10-30 08:16:53 -0600

    fix bug in fill_probability_matrix all penalty for staying in skip state on entry otherwise jumping in and then right out of skip state is same penalty as just jumping subfams..

--- parser/test_files/RunTests.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-for f in *.fa.cm.sto; do
-  echo "$f";
-  python -m polyA --seqpos --lambda .1227 "$f" ../../fixtures/25p41g_edited.matrix;
-  printf -- "-------------------------------------------------------------\n";
-done
