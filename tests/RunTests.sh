2a8765cc9da24511d3d5d06704c5a3ada1b254bc
George Lesica <george@lesica.com>
2020-12-15 10:53:18 -0700

    Undo commenting

--- tests/RunTests.sh
+++ tests/RunTests.sh
@@ -2,40 +2,40 @@
 
 set -e
 
-for f in ../fixtures/ex*.sto;
-do echo "$f";
-  m=${f%sto}matrix
-  python -m polyA "$f" "$m";
-  printf -- "-------------------------------------------------------------\n";
-done
-
-# tests confidence only option - use --lambda just so doesn't run esl_scorematrix
-for f in ../fixtures/ex*.sto;
-do echo "$f";
-  m=${f%sto}matrix
-  python -m polyA --confidence "$f" "$m";
-  printf -- "-------------------------------------------------------------\n";
-done
-
-#tests soda output
-for f in ../fixtures/ex*.sto;
-do echo "$f";
-  m=${f%sto}matrix
-  python -m polyA --sequence-position --soda --heatmap "$f" "$m";
-  ls output.*
-  rm output.*.viz;
-  rm output.*.viz.json;
-  rm output.*.heatmap;
-  printf -- "-------------------------------------------------------------\n";
-done
-
-##tests prior counts
-for f in ../fixtures/ex*cm.sto;
-do echo "$f";
-  m=${f%sto}matrix
-  python -m polyA --sequence-position --prior-counts ../fixtures/SubfamPriorCounts.txt "$f" "$m";
-  printf -- "-------------------------------------------------------------\n";
-done
+#for f in ../fixtures/ex*.sto;
+#do echo "$f";
+#  m=${f%sto}matrix
+#  python -m polyA "$f" "$m";
+#  printf -- "-------------------------------------------------------------\n";
+#done
+#
+## tests confidence only option - use --lambda just so doesn't run esl_scorematrix
+#for f in ../fixtures/ex*.sto;
+#do echo "$f";
+#  m=${f%sto}matrix
+#  python -m polyA --confidence "$f" "$m";
+#  printf -- "-------------------------------------------------------------\n";
+#done
+#
+##tests soda output
+#for f in ../fixtures/ex*.sto;
+#do echo "$f";
+#  m=${f%sto}matrix
+#  python -m polyA --sequence-position --soda --heatmap "$f" "$m";
+#  ls output.*
+#  rm output.*.viz;
+#  rm output.*.viz.json;
+#  rm output.*.heatmap;
+#  printf -- "-------------------------------------------------------------\n";
+#done
+#
+###tests prior counts
+#for f in ../fixtures/ex*cm.sto;
+#do echo "$f";
+#  m=${f%sto}matrix
+#  python -m polyA --sequence-position --prior-counts ../fixtures/SubfamPriorCounts.txt "$f" "$m";
+#  printf -- "-------------------------------------------------------------\n";
+#done
 
 for f in ../fixtures/ex*.sto;
 do echo "$f";
