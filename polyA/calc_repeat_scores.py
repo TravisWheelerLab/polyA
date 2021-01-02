427e513f47418d97b72bb239be0fd7c65ad0cbae
audreym98 <audreym98@icloud.com>
2021-01-02 11:57:42 -0800

    test for TR cut off by shard in first half of chunk, rename vars

--- polyA/calc_repeat_scores.py
+++ polyA/calc_repeat_scores.py
@@ -11,8 +11,8 @@ def calculate_repeat_scores(
     active_cells: Dict[int, List[int]],
     align_matrix: Dict[Tuple[int, int], float],
     consensus_matrix: Dict[Tuple[int, int], int],
-    chunk_start: int,
-    chunk_stop: int,
+    shard_start: int,
+    shard_stop: int,
 ) -> Dict[int, float]:
     """
     Calculates score (according to ULTRA scoring) for every segment of size
@@ -32,6 +32,8 @@ def calculate_repeat_scores(
     align_matrix
     consensus_matrix: dictionary that maps a tuple (row, col) to consensus
     position in the subfam/query sequence
+    shard_start: start seq pos of current shard
+    shard_stop: stop seq pos of current shard
 
     output:
     repeat_scores: Hash implementation of sparse 1d array. Key is int that maps col
@@ -61,17 +63,17 @@ def calculate_repeat_scores(
     >>> active_cols
     {3: [0, 1], 4: [0, 1], 5: [0, 1], 7: [0, 2], 8: [0, 2], 9: [0, 2], 10: [0, 2], 11: [0, 2], 12: [0, 2], 13: [0, 2], 14: [0, 2]}
 
-    Shard cuts off TR from right side
+    Shard cuts off TR from right side in first half of chunk
     >>> repeats = [TandemRepeat.from_json(m) for m in [{'Start': 5, 'Length': 4, 'PositionScoreDelta': '-0.5:1:1.5:1'}, {'Start': 10, 'Length': 8, 'PositionScoreDelta': '0:0.5:0.5:1.5:1.5:1:0.5:-0.5'}]]
     >>> align_mat = {}
     >>> active_cols = {}
-    >>> rep_scores = calculate_repeat_scores(repeats, 5, 4, 1, active_cols, align_mat, {}, 1, 15)
+    >>> rep_scores = calculate_repeat_scores(repeats, 5, 4, 1, active_cols, align_mat, {}, 1, 11)
     >>> rep_scores
-    {2: -0.5, 3: 1.0, 4: 1.5, 5: 1.0, 7: 0.0, 8: 0.5, 9: 0.5, 10: 1.5, 11: 1.5, 12: 1.0}
+    {2: -0.5, 3: 1.0, 4: 1.5, 5: 1.0, 7: 0.0, 8: 0.5}
     >>> align_mat
-    {(1, 2): 3.3333333333333335, (1, 3): 3.75, (1, 4): 3.75, (1, 5): 5.833333333333333, (2, 7): 1.6666666666666667, (2, 8): 3.125, (2, 9): 4.0, (2, 10): 5.0, (2, 11): 5.0, (2, 12): 4.0}
+    {(1, 2): 3.3333333333333335, (1, 3): 3.75, (1, 4): 3.75, (1, 5): 5.833333333333333, (2, 7): 1.6666666666666667, (2, 8): 3.125}
     >>> active_cols
-    {2: [0, 1], 3: [0, 1], 4: [0, 1], 5: [0, 1], 7: [0, 2], 8: [0, 2], 9: [0, 2], 10: [0, 2], 11: [0, 2], 12: [0, 2]}
+    {2: [0, 1], 3: [0, 1], 4: [0, 1], 5: [0, 1], 7: [0, 2], 8: [0, 2]}
     """
 
     repeat_scores: Dict[
@@ -90,17 +92,16 @@ def calculate_repeat_scores(
         score: float = 0
         j = 0
         k = int((chunk_size - 1) / 2)
-        # update repeat_scores for positions 0 to 15 if in boundary
+        # update repeat_scores for positions 0 to k if in shard boundary
         while j <= k and j < length:
             score += pos_scores[j]
-            if chunk_start <= start_rep + j <= chunk_stop:
+            if shard_start <= start_rep + j <= shard_stop:
                 repeat_scores[col_index + j] = pos_scores[j]
             j += 1
         window_size = j
 
-        # check TR start >= chunk start
         # update AlignMatrix and ConsensusMatrix
-        if start_rep >= chunk_start:
+        if start_rep >= shard_start:
             align_matrix[i + row_num, col_index] = (
                 score * chunk_size / window_size
             )
@@ -113,10 +114,10 @@ def calculate_repeat_scores(
 
         # calc score for the rest of the chunks
         tr_chunk_length = length
-        if rep.stop >= chunk_stop:
-            # TR ends outside of chunk boundary
+        if rep.stop >= shard_stop:
+            # TR ends outside of shard boundary
             # get length of TR before end of boundary
-            tr_chunk_length = chunk_stop - start_rep + 1
+            tr_chunk_length = shard_stop - start_rep + 1
         for j in range(1, tr_chunk_length):
             # check to remove last score in window
             if j - k - 1 >= 0:
@@ -126,11 +127,11 @@ def calculate_repeat_scores(
             if j + k < len(pos_scores):
                 score += float(pos_scores[j + k])
                 window_size += 1
-                if chunk_start <= start_rep + j + k <= chunk_stop:
+                if shard_start <= start_rep + j + k <= shard_stop:
                     # add new score to repeat_scores
                     repeat_scores[col_index + j + k] = float(pos_scores[j + k])
-            # Update matrices if in chunk boundary
-            if start_rep + j >= chunk_start:
+            # Update matrices if in shard boundary
+            if start_rep + j >= shard_start:
                 align_matrix[i + row_num, col_index + j] = (
                     score * chunk_size / window_size
                 )
