1392595ec8dff9a6b7fe4a11360d33df20a8187f
George Lesica <george@lesica.com>
2021-03-01 12:45:36 -0700

    Spruce up edges a bit

--- polyA/edges.py
+++ polyA/edges.py
@@ -6,23 +6,23 @@ def edges(starts: List[int], stops: List[int]) -> Tuple[int, int]:
     Find and return the min start and max stop positions for the entire region
     included in the alignments.
 
-    input:
-    starts: start positions on the target sequence from the input alignment
-    stops: stop positions on the target sequence from the input alignment
+    Inputs:
 
-    output:
+    starts - start positions on the target sequence from the input alignment
+    stops - stop positions on the target sequence from the input alignment
+
+    Outputs:
+
     minimum and maximum start and stop positions on the chromosome/target
     sequences for whole alignment
 
-    >>> starts = [0, 1, 4, 7]
-    >>> stops = [0, 3, 10, 9]
-    >>> b, e = edges(starts, stops)
+    >>> b, e = edges(starts=[0, 1, 4, 7], stops=[0, 3, 10, 9])
     >>> b
     1
     >>> e
     10
     """
-    min_start: int = min(*starts[1:])
-    max_stop: int = max(*stops[1:])
+    min_start: int = min(starts[1:])
+    max_stop: int = max(stops[1:])
 
     return min_start, max_stop
